c6b53bc11e4a9f000968a8ca649f84cc1e121dbe
Manish Shegokar <shegokarm@gmail.com>
2017-11-19 00:15:12 +0000

    Updated

--- houseprices1.py
+++ houseprices1.py
@@ -1,3 +1,4 @@
+# Importing libraries
 import pandas as pd
 import numpy as np
 import seaborn as sns
@@ -6,10 +7,11 @@ import matplotlib.pyplot as plt
 from scipy.stats import skew
 import os 
 
-
+# Changing directory
 os.getcwd()
-os.chdir("E:\Python_Dataset\house prices\housing")
+os.chdir("/Users/manishshegokar/Documents/Manish_documents/My independent projects/Python_Dataset/house prices/housing")
 
+# Importing train and test data files
 train = pd.read_csv("train.csv")
 
 test = pd.read_csv("test.csv")
@@ -19,6 +21,7 @@ all_data = pd.concat((train.loc[:,'MSSubClass':'SaleCondition'],
                       test.loc[:,'MSSubClass':'SaleCondition']))
 #Data Preprocessing
 train['SalePrice'].hist()
+
 #creating data frame with old saleprice having name as prices and new salprice 
 #with log name
 prices = pd.DataFrame({"price":train["SalePrice"], 
@@ -45,25 +48,35 @@ X_train = all_data[:train.shape[0]]
 X_test = all_data[train.shape[0]:]
 y = train.SalePrice
 
-from sklearn.linear_model import Ridge, RidgeCV, ElasticNet, LassoCV, LassoLarsCV
+# Importing libraries
+from sklearn.linear_model import Ridge, RidgeCV, LassoCV, LassoLarsCV
 from sklearn import cross_validation
 
+# Function to return rmse
 def rmse_cv(model):
     rmse= np.sqrt(-cross_validation.cross_val_score(model, X_train, y, scoring="mean_squared_error", cv = 5))
     return(rmse)
 
 model_ridge = Ridge()
 
+# Various values for alpha
 alphas = [0.05, 0.1, 0.3, 1, 3, 5, 10, 15, 30, 50, 75]
+
 cv_ridge = [rmse_cv(Ridge(alpha = alpha)).mean() 
             for alpha in alphas]
 
+# fitting model on train dataset
 newridge = Ridge(alpha=5).fit(X_train,y)
 rmse_cv(newridge).mean()
+
+# Predicting test dataset
 p = np.expm1(newridge.predict(X_test))
 result = pd.DataFrame({"ID":test.Id,"SalePrice":p})
+
+# Saving result into result.csv
 result.to_csv("result.csv",index=False)
 
+# Plot
 cv_ridge = pd.Series(cv_ridge, index = alphas)
 cv_ridge.plot(title = "Validation - Just Do It")
 plt.xlabel("alpha")
@@ -71,6 +84,7 @@ plt.ylabel("rmse")
 
 cv_ridge.min()
 
+# Lasso regression
 model_lasso = LassoCV(alphas = [1, 0.1, 0.001, 0.0005]).fit(X_train, y)
 
 rmse_cv(model_lasso).mean()
@@ -81,15 +95,20 @@ print("Lasso picked " + str(sum(coef != 0)) + " variables and eliminated the oth
 imp_coef = pd.concat([coef.sort_values().head(10),
                      coef.sort_values().tail(10)])
                      
-#let's look at the residuals as well:
+# residuals
 matplotlib.rcParams['figure.figsize'] = (6.0, 6.0)
 
+# Calculating residuals of train set
 preds = pd.DataFrame({"preds":model_lasso.predict(X_train), "true":y})
 preds["residuals"] = preds["true"] - preds["preds"]
+
+# Plot
 preds.plot(x = "preds", y = "residuals",kind = "scatter")                     
-                     
+              
+# Predicting test dataset using lasso       
 preds = np.expm1(model_lasso.predict(X_test))
 
+# Saving the result
 solution = pd.DataFrame({"id":test.Id, "SalePrice":preds})
-solution.to_csv("ridge_sol.csv", index = False)                     
+solution.to_csv("sol.csv", index = False)                     
 
